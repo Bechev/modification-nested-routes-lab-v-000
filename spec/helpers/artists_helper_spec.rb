5212edfd7fff899dc8dcca8a811a94fc7d3d157d
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-08-01 19:03:13 +0000

    automatically backed up by learn

--- spec/helpers/artists_helper_spec.rb
+++ spec/helpers/artists_helper_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe ArtistsHelper, type: :helper do
   end
 
   # subject { helper }
-  # 
+  #
   # it { should respond_to(:display_artist).with(1).argument }
   # it { should respond_to(:artist_select).with(2).arguments }
 
