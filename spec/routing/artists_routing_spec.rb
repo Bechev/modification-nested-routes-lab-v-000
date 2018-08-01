74d38482b07261b395319f4c6f1755fa5ec1ce14
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-08-01 19:03:09 +0000

    automatically backed up by learn

--- spec/routing/artists_routing_spec.rb
+++ spec/routing/artists_routing_spec.rb
@@ -14,11 +14,11 @@ RSpec.describe "artists routing", type: :routing do
   # it "routes to a song as a nested resource of an artist" do
   #   expect(get: artist_song_path(1, 2)).to route_to("songs#show", artist_id: "1", id: "2")
   # end
-  # 
+  #
   # it "routes to new song as a nested resource of an artist" do
   #   expect(get: new_artist_song_path(1)).to route_to("songs#new", artist_id: "1")
   # end
-  # 
+  #
   # it "routes to edit song as a nested resource of an artist" do
   #   expect(get: edit_artist_song_path(1,2)).to route_to("songs#edit", artist_id: "1", id: "2")
   # end
